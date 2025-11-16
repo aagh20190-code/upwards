c99e585ea039a3715b6eb59c3fe234eb4cd4e619
aagh2019 <aagh20190@gmail.com>
2025-11-17 00:17:39 +0600

    updatedTabButton

---
+++ lib/common_widget/tab_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:upwards2/common/colo_extension.dart';
+
+class TabButton extends StatelessWidget {
+  final String icon;
+  final String selectIcon;
+  final VoidCallback onTap;
+  final bool isActive;
+  const TabButton({super.key,
+    required this.icon,
+    required this.selectIcon,
+    required this.onTap,
+    required this.isActive,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(onTap: onTap,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(isActive ? selectIcon : icon,
+                    width: 35,
+                    height: 35,
+                    fit: BoxFit.fitWidth),
+                    SizedBox(
+                      height: isActive ? 8 : 12,
+                    ),
+                    if(isActive)
+                    Container(
+                      width: 4,
+                      height: 4,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: TColor.secondaryGrad),
+                        borderRadius: BorderRadius.circular(2),
+                      ),
+                    )
+                  ],
+                ),);
+  }
+}
